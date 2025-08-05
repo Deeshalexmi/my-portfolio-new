3f6119431dd0e63802b60f7e47ef0b993ab4be31
Deeshalexmi <deesha.chandran0424@gmail.com>
2025-08-05 22:16:36 +0800

    Deploy fixed site to GitHun Pages via docs

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import { componentTagger } from "lovable-tagger";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    base: '/my-portfolio-new/',
+    base: 'my-portfolio-new',
     host: "::",
     port: 8080,
   },
